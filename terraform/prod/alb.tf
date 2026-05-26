ba15ec8e96dee43593e8568ed062e1cccb1339eb
Will Myers <wmyers@housecanary.com>
2026-05-26 14:36:43 -0700

    feat(terraform/prod): ALB + target group + HTTPS listener + HTTP redirect

---
+++ terraform/prod/alb.tf
@@ -0,0 +1,59 @@
+resource "aws_lb" "main" {
+  name               = "${var.app_name_prefix}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = aws_subnet.public[*].id
+
+  idle_timeout = 60
+  enable_http2 = true
+
+  tags = { Name = "${var.app_name_prefix}-alb" }
+}
+
+resource "aws_lb_target_group" "api" {
+  name        = "${var.app_name_prefix}-api"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.main.id
+
+  health_check {
+    path                = "/healthz"
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    interval            = 15
+    timeout             = 5
+    matcher             = "200"
+  }
+
+  deregistration_delay = 30
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate_validation.api.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api.arn
+  }
+}
+
+resource "aws_lb_listener" "http_redirect" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
