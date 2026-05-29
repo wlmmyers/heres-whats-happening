702873f64f2739258696ec46eb28c5aa4754ef1c
William Myers <wlmmyers@gmail.com>
2026-05-28 17:08:51 -0700

    Gitignore more files

--- terraform/prod/backend.tf
+++ terraform/prod/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     # Fill in <ACCOUNT_ID> after Plan 7's bootstrap apply prints `tf_state_bucket`.
-    # Or pass via: terraform init -backend-config="bucket=hwh-tf-state-<ACCOUNT_ID>"
-    bucket         = "hwh-tf-state-REPLACE_WITH_ACCOUNT_ID"
+    # Or pass via: terraform init -backend-config="bucket=hwh-tf-state-435432261718"
+    bucket         = "hwh-tf-state-435432261718"
     key            = "prod/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "hwh-tf-state-lock"
