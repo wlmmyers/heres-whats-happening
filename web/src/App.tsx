30bff74e498c6857cd3c3e786d5fc81a4d50101e
William Myers <wlmmyers@gmail.com>
2026-07-23 09:29:11 -0700

    Fix LandingPage build error

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import RequireAuth from './auth/RequireAuth';
 import Layout from './components/Layout';
 import InterestsPage from './pages/InterestsPage';
